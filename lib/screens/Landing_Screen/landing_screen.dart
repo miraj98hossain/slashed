f0bf61ca9481aed4b1f4bb6801d744eb7e3ab2e9
Miraj Hossain <miraj09hossain@gmail.com>
2024-01-15 22:45:03 +0600

    Listview Ui

--- lib/screens/Landing_Screen/landing_screen.dart
+++ lib/screens/Landing_Screen/landing_screen.dart
@@ -15,6 +15,7 @@ class LandingScreen extends StatelessWidget {
           margin:
               const EdgeInsets.only(top: 20, left: 20, right: 10, bottom: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -95,6 +96,140 @@ class LandingScreen extends StatelessWidget {
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Select Category",
+                style: GoogleFonts.dmSans(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  letterSpacing: -0.35,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 100,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Container(
+                            height: 71,
+                            width: 71,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12.0),
+                              color: const Color(0xFFF0FAF2),
+                              border: Border.all(
+                                width: 2,
+                                color: const Color(0xFF4FBF67),
+                              ),
+                            ),
+                            child: const Center(
+                              child: Image(
+                                image:
+                                    AssetImage("assets/icons/Food_Drink01.png"),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Burgers",
+                            style: GoogleFonts.dmSans(
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                                fontStyle: FontStyle.normal),
+                          )
+                        ],
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(width: 10);
+                    },
+                    itemCount: 5),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Feature Partners",
+                style: GoogleFonts.dmSans(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  letterSpacing: -0.35,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 133,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 133,
+                        width: 230,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(7.0),
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            const Positioned(
+                              child: Image(
+                                image: AssetImage(
+                                    "assets/images/Feature_partner.png"),
+                              ),
+                            ),
+                            Positioned(
+                              left: 9,
+                              top: 10,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 25,
+                                    width: 60,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 6),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: const Color(0xFFFFB132),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "Featured",
+                                        style: GoogleFonts.dmSans(
+                                          color: Colors.white,
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.w700,
+                                          fontStyle: FontStyle.normal,
+                                          letterSpacing: -0.3,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(width: 10);
+                    },
+                    itemCount: 5),
+              )
             ],
           ),
         ),
